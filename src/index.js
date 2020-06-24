624fc888552a70324e20f289b589485e3cd81788
Anas Shad <anasshad@hotmail.com>
2020-06-24 15:16:45 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+dotenv.config({ path: __dirname + "/.env" });
+
+const transactions = require("./routes/transactions");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/transactions", transactions);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} at port ${PORT}`.yellow.bold
+  )
+);
